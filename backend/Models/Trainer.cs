87bccd4e8610cac4cc444bcfee03078484bb78f8
Omar Ahmed <omarsomaa55@gmail.com>
2025-04-28 00:59:54 +0300

    fix/backend/update_models_with_new_schema

---
+++ backend/Models/Trainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gym_project.Models;
+
+public partial class Trainer
+{
+    public int TrainerId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public virtual ICollection<ProgressReport> ProgressReports { get; set; } = new List<ProgressReport>();
+
+    public virtual ICollection<Session> Sessions { get; set; } = new List<Session>();
+
+    public virtual User TrainerNavigation { get; set; } = null!;
+
+    public virtual ICollection<TrainingPlan> TrainingPlans { get; set; } = new List<TrainingPlan>();
+}
