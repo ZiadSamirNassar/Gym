81611012c03afbdfe4c70e7955cbe23fdddeebad
ziadehab433 <ziadelsherif7@gmail.com>
2025-05-14 14:57:22 +0300

    messages css fixed

--- frontend/src/pages/trainer/TrainerClients.jsx
+++ frontend/src/pages/trainer/TrainerClients.jsx
@@ -3,6 +3,16 @@ import ClientCard from "../../components/ClientCard";
 import { useState, useEffect } from "react";
 
 const TrainerClients = () => {
+  localStorage.setItem(
+    "token",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjMiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWhtZWQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ0cmFpbmVyIiwiZXhwIjoxNzQ3MjYzNTExLCJpc3MiOiJNeUd5bUFwcCIsImF1ZCI6Ik15R3ltQXBwQXVkaWVuY2UifQ.QzNDZ7LbYM2AhStJ8wOclKK3Ebuxo2Vj_o2tUzdYk3E"
+  );
+
+  localStorage.setItem(
+    "type",
+    "trainer"
+  );
+
   const [clients, setClients] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
